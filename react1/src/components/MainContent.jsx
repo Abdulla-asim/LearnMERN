f888e6e86b4650a62f75518d7f64716e27e1027d
Abdullah <abdullah.bscs23seecs@seecs.edu.pk>
2025-04-21 16:02:48 +0500

    new image

--- react1/src/components/MainContent.jsx
+++ react1/src/components/MainContent.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import UserProfile from './UserProfile';
+import Welcome from './Welcome';
+import Time from './Time';
 
 const MainContent = () => {
   return (
     <div className='mainContent'>
-        <h1>
-            Welcome!
-        </h1>
+        <Welcome/>
+        <Time/>
         <UserProfile name='Abdullah' age='20' location='NUST'/>
     </div>
   )
